bbd3c02b2fd07e1f0175283909ed95b220da6c48
TuomasHirv <hirvonentume@gmail.com>
2025-12-01 17:44:10 +0200

    Refracored tennis_game.py

--- viikko5/tennis/src/tennis_game.py
+++ viikko5/tennis/src/tennis_game.py
@@ -2,54 +2,72 @@ class TennisGame:
     def __init__(self, player1_name, player2_name):
         self.player1_name = player1_name
         self.player2_name = player2_name
-        self.m_score1 = 0
-        self.m_score2 = 0
+        self.player1_score = 0
+        self.player2_score = 0
+        
+        self.match_point = 4
+        self.zero_points_text = "Love"
+        self.one_points_text = "Fifteen"
+        self.two_points_text = "Thirty"
+        self.three_points_text = "Forty"
+        self.match_point_tie_text = "Deuce"
 
     def won_point(self, player_name):
         if player_name == "player1":
-            self.m_score1 = self.m_score1 + 1
+            self.player1_score = self.player1_score + 1
         else:
-            self.m_score2 = self.m_score2 + 1
+            self.player2_score = self.player2_score + 1
 
     def get_score(self):
-        score = ""
-        temp_score = 0
-
-        if self.m_score1 == self.m_score2:
-            if self.m_score1 == 0:
-                score = "Love-All"
-            elif self.m_score1 == 1:
-                score = "Fifteen-All"
-            elif self.m_score1 == 2:
-                score = "Thirty-All"
-            else:
-                score = "Deuce"
-        elif self.m_score1 >= 4 or self.m_score2 >= 4:
-            minus_result = self.m_score1 - self. m_score2
-
-            if minus_result == 1:
-                score = "Advantage player1"
-            elif minus_result == -1:
-                score = "Advantage player2"
-            elif minus_result >= 2:
-                score = "Win for player1"
-            else:
-                score = "Win for player2"
+        score_information = ""
+        if self.player1_score == self.player2_score:
+            score_information = self.text_for_tasatilanne()
+        elif self.player1_score >= self.match_point or self.player2_score >= self.match_point:
+            score_information = self.text_for_advantage()
         else:
-            for i in range(1, 3):
-                if i == 1:
-                    temp_score = self.m_score1
-                else:
-                    score = score + "-"
-                    temp_score = self.m_score2
+            score_information = self.normal_score_text()
+        return score_information
 
-                if temp_score == 0:
-                    score = score + "Love"
-                elif temp_score == 1:
-                    score = score + "Fifteen"
-                elif temp_score == 2:
-                    score = score + "Thirty"
-                elif temp_score == 3:
-                    score = score + "Forty"
+    def text_for_tasatilanne(self):
+        return_text = ""
+        if self.player1_score == 0:
+            return_text = self.zero_points_text+"-All"
+        elif self.player1_score == 1:
+            return_text = self.one_points_text+"-All"
+        elif self.player1_score == 2:
+            return_text = self.two_points_text+"-All"
+        else:
+            return_text = self.match_point_tie_text
+        return return_text
+        
+    def text_for_advantage(self):
+        return_text = ""
+        advantage = self.player1_score - self.player2_score
+        if advantage == 1:
+            return_text = "Advantage "+self.player1_name
+        elif advantage == -1:
+            return_text = "Advantage "+self.player2_name
+        elif advantage >= 2:
+            return_text = "Win for "+self.player1_name
+        else:
+            return_text = "Win for "+self.player2_name
+        return return_text
+    def normal_score_text(self):
+        return_text = ""
+        temp_score = 0
+        for i in range(1, 3):
+            if i == 1:
+                temp_score = self.player1_score
+            else:
+                return_text = return_text + "-"
+                temp_score = self.player2_score
+            if temp_score == 0:
+                return_text = return_text + self.zero_points_text
+            elif temp_score == 1:
+                return_text = return_text + self.one_points_text
+            elif temp_score == 2:
+                return_text = return_text + self.two_points_text
+            elif temp_score == 3:
+                return_text = return_text + "Forty"
+        return return_text
 
-        return score
